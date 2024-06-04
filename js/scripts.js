13f182e893a85ceb0cef33d4c945fdcf5596ac73
NanhoiRey <nanhoirey94@gmail.com>
2024-06-04 15:56:07 -0300

    correciones nav y about

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,16 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+document.addEventListener('scroll', () =>{
+    const navbar = document.querySelector('.navbar');
+
+        if (window.scrollY > 0){
+            navbar.classList.add('scrolled');
+        } 
+        else {
+            navbar.classList.remove('scrolled');
+        }
+
+
+    });
